5a332a6855b4401926e5b375f9204357e4f1a6d0
Amritanshu <64033297+Amritanshu005@users.noreply.github.com>
2026-05-16 00:54:12 +0530

    extends

---
+++ testxxiv.java
@@ -0,0 +1,28 @@
+class A{
+    public void show(){
+        System.out.println("in A show");
+    }
+    public void config(){
+        System.out.println("in A config");
+    }
+}
+
+class B extends A{
+    public void show(){ // method overwritting
+        System.out.println("in B show");
+    }
+}
+
+
+
+
+public class testxxiv {
+    public static void main(String[] args) {
+        B obj = new B(); // LHS refrence, RHS object  
+        // we can also create a refrence from parent class.... and object from sub class
+        obj.show();
+        obj.config();
+    }
+}
+    
+
